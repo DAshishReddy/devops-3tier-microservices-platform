bd45dc95c973069de103f63668ed6290a9395d29
Ashish <54053045+DAshishReddy@users.noreply.github.com>
2026-03-15 15:31:01 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,15 @@
 const router = require("express").Router()
 const User = require("../models/user")
-const jwt = require("jsonwebtoken")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
-router.post("/register", async (req,res)=>{
+router.post("/register",async(req,res)=>{
 
- const hashedPassword = bcrypt.hashSync(req.body.password,8)
+ const hashed = bcrypt.hashSync(req.body.password,8)
 
  const user = new User({
-  username: req.body.username,
-  password: hashedPassword
+  username:req.body.username,
+  password:hashed
  })
 
  await user.save()
@@ -17,19 +17,19 @@ router.post("/register", async (req,res)=>{
  res.send("User registered")
 })
 
-router.post("/login", async (req,res)=>{
+router.post("/login",async(req,res)=>{
 
  const user = await User.findOne({username:req.body.username})
 
  if(!user) return res.status(404).send("User not found")
 
- const passwordValid = bcrypt.compareSync(req.body.password,user.password)
+ const valid = bcrypt.compareSync(req.body.password,user.password)
 
- if(!passwordValid) return res.status(401).send("Invalid password")
+ if(!valid) return res.status(401).send("Invalid password")
 
  const token = jwt.sign({id:user._id},"devops-secret",{expiresIn:86400})
 
  res.json({token})
 })
 
-module.exports = router  
+module.exports = router
